fa74e4fc50aa7463da5abafa8096a7d1defeb206
Ansh <anshgujral1997@gmail.com>
2018-07-22 19:38:51 +0530

    added add_node functionality (first step of decntralization)

--- zCoin.py
+++ zCoin.py
@@ -156,5 +156,19 @@ def add_transaction():
     response = {'message': f'this transaction will be added to block {index}'}
     return jsonify(response), 201
 
+# Decentralization
+# Connecting new nodes
+@app.route('/connect_node', methods = ['POST'])
+def connect_node():
+    json = request.get_json()
+    nodes = json.get('nodes')
+    if nodes is None:
+        return "No node", 400
+    for node in nodes:
+        blockchain.add_node(node)
+    response = {'message': 'All the nodes are now connected. The Hadcoin Blockchain now contains the following nodes:',
+                'total_nodes': list(blockchain.nodes)}
+    return jsonify(response), 201
+
 # Running the app
 app.run(host = '0.0.0.0', port = 5000)
